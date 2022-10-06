f754ace618f2932b642977038874ac4bd848717e
SelahattinKarakaya <selahattin.karakaya@nexus-ips.de>
2022-10-06 17:30:56 +0200

    all iterable functions

--- ansible.py
+++ ansible.py
@@ -1,12 +1,3 @@
-def count_users(group):
-  count = 0
-  for member in get_members(group):
-    if is_group(member):
-      count+=count_users(member)
-      continue
-    count += 1
-  return count
-
-print(count_users("sales")) # Should be 3
-print(count_users("engineering")) # Should be 8
-print(count_users("everyone")) # Should be 18
+for x in range(10):
+    for y in range(x):
+        print(y)
