6c061303a5b293574f4e686a5764dabe250fa5a0
SelahattinKarakaya <selahattin.karakaya@nexus-ips.de>
2022-10-07 16:28:14 +0200

    first_and_last

---
+++ Strings/first_and_last.py
@@ -0,0 +1,15 @@
+# Modify the first_and_last function 
+# so that it returns 
+# True if the first letter of the string is the same as the last letter of the string, 
+# False if they’re different. 
+# Remember that you can access characters 
+# using message[0] or message[-1].
+
+def first_and_last(message):
+    if (len(message)==0 or message[0]==message[-1]):
+        return True
+    return False
+
+print(first_and_last("else"))
+print(first_and_last("tree"))
+print(first_and_last(""))
